b20b2dcb4a9e31895d91881bc26de07bd1bc4c01
IC <ic.kmitl@gmail.com>
2013-08-30 11:57:04 +0700

    1st problem v2

--- src/svv/pkg01/SVV01.java
+++ src/svv/pkg01/SVV01.java
@@ -13,43 +13,31 @@ public class SVV01 {
     /**
      * @param args the command line arguments
      */
-    public static void checkTriType(double a,double b,double c)
+    public static String checkTriType(double a,double b,double c)
     {
         if((a==0)|(b==0)||(c==0))
         {
-            System.out.println("Invalid");
+            return "Invalid";
         }
         else if((a+b<c)||(a+c<b)||(b+c<a))
         {
-            System.out.println("Invalid");
+            return "Invalid";
         }
         else 
         {
             if ((a==b)&&(a==c))
             {
-                System.out.println("Equilateral Triangle");
+                return "Equilateral Triangle";
             }
             else if((a==b)||(a==c)||(b==c))
             {
-            System.out.println("Isosceles Triangle");
+                return "Isosceles Triangle";
             }
             else
             {
-                System.out.println("Scalene");
+               return "Scalene";
             }
         }
         
-    }
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("FirstTry : "); 
-        checkTriType(5,5,5);
-        System.out.print("SecondTry : "); 
-        checkTriType(5,5,9);
-        System.out.print("ThirdTry : "); 
-        checkTriType(3,4,5);
-        System.out.print("ForthTry : "); 
-        checkTriType(5,5,500);
     }
 }
---
+++ src/svv/pkg01/SVV02.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package svv.pkg01;
+
+/**
+ *
+ * @author IC
+ */
+public class SVV02 {
+    public static void nextDate(int d,int m,int y)
+    {
+        if()
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("eiei");
+    }
+}
